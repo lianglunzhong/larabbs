c949b6250db49824e346927385e2e913a4292040
vagrant <vagrant@homestead>
2018-12-10 08:23:41 +0000

    spatie/laravel-backup:数据备份

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,6 +27,14 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
 
+        // spatie/laravel-backup 数据备份定时任务
+        if (app()->environment('production')) {
+            $schedule->command('backup:run')->cron('0 */4 * * *'); // 每 4 个小时执行一次备份
+            $schedule->command('backup:monitor')->dailyAt('03:00'); // 每天凌晨 3 点执行一次环境监控
+            $schedule->command('backup:clean')->dailyAt('03:10'); // 每天凌晨 3 点10 分执行一次备份清理
+        }
+
+
         // 一小时执行一次 「活跃用户」 数据生成的命令
         $schedule->command('larabbs::calculate-actice-user')->hourly();
         // 每日零时执行一次
