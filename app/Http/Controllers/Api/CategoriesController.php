6034e9164202a355af8aff42e3fed6ed7d0b80c1
vagrant <vagrant@homestead>
2018-12-18 08:02:21 +0000

    add kalnoy/nestedset

--- app/Http/Controllers/Api/CategoriesController.php
+++ app/Http/Controllers/Api/CategoriesController.php
@@ -5,12 +5,20 @@ namespace App\Http\Controllers\Api;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Transformers\CategoryTransformer;
-
+use Dingo\Api\Transformer\Factory;
 
 class CategoriesController extends Controller
 {
-    public function index()
+    public function index(Request $request, Factory $transformerFactory)
     {
-        return $this->response->collection(Category::all(), new CategoryTransformer());
+        if ($request->include == 'children') {
+            $categories = Category::defaultOrder()->get()->toTree();
+            // 关闭 Dingo 的预加载
+            $transformerFactory->disableEagerLoading();
+        } else {
+            $categories = Category::whereIsRoot()->defaultOrder()->get();
+        }
+
+        return $this->response->collection($categories, new CategoryTransformer());
     }
 }
