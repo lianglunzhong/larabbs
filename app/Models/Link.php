5e68cde3f7e97131e253e3755c0b261f3e28d209
vagrant <vagrant@homestead>
2019-01-09 08:33:00 +0000

    spatie/eloquent-sortable

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -2,8 +2,21 @@
 
 namespace App\Models;
 
-class Link extends Model
+use Spatie\EloquentSortable\Sortable;
+use Spatie\EloquentSortable\SortableTrait;
+
+class Link extends Model implements Sortable
 {
+    use SortableTrait {
+        scopeOrdered as sorttableScopeOrdered;
+    }
+
+
+    public $sortable = [
+        'order_column_name' => 'order',
+        'sort_when_creating' => true,
+    ];
+
     protected $fillable = ['title', 'link'];
 
     // public $cache_key = 'larabbs_links';
@@ -12,6 +25,11 @@ class Link extends Model
 
     public function getAllCached()
     {
-        return $this->remember($this->cache_expire_in_minutes)->get();
+        return $this->remember($this->cache_expire_in_minutes)->ordered()->get();
+    }
+
+    public function scopeOrdered($query)
+    {
+        return $this->sorttableScopeOrdered($query);
     }
 }
